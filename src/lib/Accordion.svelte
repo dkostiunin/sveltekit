063a98a8f8d5734e5bb842f8e64e50b77f6dcc7f
Dmitrii Kostiunin <kostiunindima1@gmail.com>
2022-06-07 20:42:16 +0300

    07062042

--- src/lib/Accordion.svelte
+++ src/lib/Accordion.svelte
@@ -7,18 +7,18 @@
   
   <div class="accordion">
       
-      <div class="header">
-          <div class="text"><slot name="head"></slot>	</div>		
-          <button on:click={handleClick}>+/-</button>
+      <div class="header" on:click={handleClick}>
+          <div class="text"><slot name="head"></slot></div>
+          <svg class:rotate="{open}" viewBox="0 0 24 24"><path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></svg>
       </div>
       
       {#if open}
           <div class="details" transition:slide><slot name="details"></slot></div>
       {/if}
       
-      <div class="header">
-          <div class="text"><slot name="head2"></slot>	</div>		
-          <button on:click={handleClick2}>+/-</button>
+      <div class="header" on:click={handleClick2}>
+          <div class="text"><slot name="head2"></slot></div>
+          <svg class:rotate="{open2}" viewBox="0 0 24 24"><path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></svg>
       </div>
       
       {#if open2}
@@ -48,8 +48,32 @@
         padding: 1rem;
         margin-top: -1.1rem;
         margin-bottom: 1.5rem;
-        margin-left: 1rem;
-        margin-right: 0rem;
+        margin-left: 0;
+        margin-right: -1.2rem;
         background-color: #e1e6eb;
       }
+
+      svg{
+              -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    width: 1em;
+    height: 1em;
+    display: inline-block;
+    fill: #0000008a;
+    -webkit-flex-shrink: 0;
+    -ms-flex-negative: 0;
+    flex-shrink: 0;
+    -webkit-transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    font-size: 1.5rem;
+      }
+
+      .rotate{
+          transform: rotate(180deg);
+          -webkit-transition: -webkit-transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+           transition: transform 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+        }
+
   </style>
