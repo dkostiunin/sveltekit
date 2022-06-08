0ba12a0df33f066aa338bc7e9026192cccdba123
Dmitrii Kostiunin <kostiunindima1@gmail.com>
2022-06-08 11:28:32 +0300

    08061128

--- src/routes/upload.svelte
+++ src/routes/upload.svelte
@@ -1,9 +1,9 @@
-<script context="module">
+<!-- <script context="module">
   import { browser, dev } from '$app/env';
 	export const hydrate = dev;
 	export const router = browser;
 	export const prerender = true;
-</script>
+</script> -->
 
 <script>
   let images
