7e6fb0d8a16f1957dbb07d632f43eb38408d44d4
Dmitrii Kostiunin <kostiunindima1@gmail.com>
2022-07-03 13:03:00 +0300

    030720221302

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -3,28 +3,37 @@ import nodemailer from 'nodemailer';
 // async..await is not allowed in global scope, must use a wrapper
 export async function mail(body) {
     console.log(223344,body)
-  let testAccount = await nodemailer.createTestAccount();
-  let transporter = nodemailer.createTransport({
-    ///host: "smtp.ethereal.email",
-	host: "smtp.yandex.ru",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-        user: 'darom@darom.tk',
-        pass: 'roge816013'
-    },
-  });
+  try {
 
-  let info = await transporter.sendMail({
-    from: '"Dmitrii Kostiunin" <darom@darom.tk>', // sender address
-    to: "melochevka@melochevka.ru, 1@melochevka.ru", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text:body, // plain text body
-    html:body, // html body
-  });
+      let testAccount = await nodemailer.createTestAccount();
+      let transporter = nodemailer.createTransport({
+        ///host: "smtp.ethereal.email",
+    //	host: "smtp.yandex.ru",
+      host: "smtp.mail.ru",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          //  user: 'darom@darom.tk',
+            user: 'dimak00@internet.ru',
+            //pass: 'roge816013'
+            pass: 'WPKhp4fz4coWoPWGXK3u'
+        },
+      });
 
-  console.log("Message sent: %s", info.messageId);
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      let info = await transporter.sendMail({
+        from: '"Dmitrii Kostiunin" <dimak00@internet.ru>', // sender address
+        to: "1@melochevka.ru", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text:body, // plain text body
+        html:body, // html body
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-mail().catch(console.error);
+
+//mail().catch(console.error);
--- src/routes/api/basket.js
+++ src/routes/api/basket.js
@@ -3,7 +3,12 @@ import {mail} from '$lib/nodemailer';
 export async function post({ params, request }) {
    const body = await request.json() // or request.json(), etc
     //await do_something_with(params, body);
-    mail(body.phone)
+    try {
+      mail(body.phone)
+    } catch (error) {
+      console.log(error)
+    }
+   
     console.log(body)
    
     return { status: 201 };
